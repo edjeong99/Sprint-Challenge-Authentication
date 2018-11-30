9657c2895c007ad0353206a7f99224e185f574ab
Edward J <edjeong99@gmail.com>
2018-11-30 09:45:57 -0500

    implement login

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,6 +5,7 @@ const jwtKey = require('../_secrets/keys').jwtKey;
 // quickly see what this file exports
 module.exports = {
   authenticate,
+  generateToken
 };
 
 // implementation details
@@ -24,4 +25,16 @@ function authenticate(req, res, next) {
       error: 'No token provided, must be set on the Authorization Header',
     });
   }
+}
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  }
+  const options = {
+    expiresIn: '10m',
+  };
+
+  return jwt.sign(payload, jwtKey, options);
 }
--- config/routes.js
+++ config/routes.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const db = require('../database/dbConfig.js');
 
-const { authenticate } = require('./middlewares');
+const { authenticate, generateToken } = require('./middlewares');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -27,6 +27,20 @@ function register(req, res) {
 
 function login(req, res) {
   // implement user login
+  const cred = req.body;
+
+  db('users')
+  .where({username : cred.username})
+  .first()
+  .then( user => {
+    if(user && bcrypt.compareSync(cred.password, user.password)){
+      const token = generateToken(user);
+      res.status(200).json(token);
+    }else
+      res.status(401).json({message:"Wrong username/password"})
+    })
+  .catch( () => res.status(500).json({message:"Login Failed!"}))
+
 }
 
 function getJokes(req, res) {
