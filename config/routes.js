c0252a7d120ddc3f1f083908f17042ace2a62e5c
Edward J <edjeong99@gmail.com>
2018-11-30 09:17:15 -0500

    implement register

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,28 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const db = require('../database/dbConfig.js');
 
 const { authenticate } = require('./middlewares');
 
 module.exports = server => {
   server.post('/api/register', register);
   server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  // server.get('/api/jokes', authenticate, getJokes);
+  server.get('/api/jokes',  getJokes);
+  server.get('/api/users', getUsers);
 };
 
 function register(req, res) {
   // implement user registration
+  const newUser = req.body;
+  newUser.password = bcrypt.hashSync(newUser.password, 3);
+
+  db('users')
+  .insert(newUser)
+  .then( id => res.status(200).json(id))
+  .catch(err => res.status(500).json({message:"could not register the user", err}))
+
 }
 
 function login(req, res) {
@@ -28,3 +41,11 @@ function getJokes(req, res) {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
 }
+const getUsers = (req, res, next) => {
+
+  db('users')
+      .select('*')
+      .then(users =>{res.status(200).json(users)})
+      .catch((err)=>
+res.status(500).json({ message: 'could not get users', err }))
+};
